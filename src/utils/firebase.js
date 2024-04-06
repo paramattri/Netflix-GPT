4a4ee834adf36073bb79a4b380465a89e62cd06b
Param Attri <attri.param@gmail.com>
2024-04-06 19:45:00 +0530

    configures the app with firebase and deploys it to firebase

---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBqlSHj-xPv6SvoLk5VpoeYI9pkRANQVCQ",
+  authDomain: "netflixgpt-ae689.firebaseapp.com",
+  projectId: "netflixgpt-ae689",
+  storageBucket: "netflixgpt-ae689.appspot.com",
+  messagingSenderId: "253833187646",
+  appId: "1:253833187646:web:0c73107342a0eea8dd9c6f",
+  measurementId: "G-0N5Q33ZL4P",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
