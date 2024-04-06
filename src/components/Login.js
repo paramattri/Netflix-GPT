252ca3bd4c5c36c217e833ec92a2ed5f66169eac
Param Attri <attri.param@gmail.com>
2024-04-06 19:06:43 +0530

    implements form validation for signin/signup

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,26 @@
-import React, { useState } from "react";
+import { useState, useRef } from "react";
+import { validateData } from "../utils/validate";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const [validationErrorMessage, setValidationErrorMessage] = useState(null);
+
+  const name = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
 
   const toggleSignInForm = () => {
     setIsSignInForm(!isSignInForm);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    const errorMessage = validateData(
+      email.current.value,
+      password.current.value
+    );
+    setValidationErrorMessage(errorMessage);
   };
 
   return (
@@ -13,24 +29,28 @@ const Login = () => {
         <p className="text-white mb-7 text-[32px] font-medium">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </p>
-        <form className="">
+        <form className="" onSubmit={handleFormSubmit}>
           {!isSignInForm && (
             <input
               className="w-full px-5 pt-4 h-[50px] rounded-[4px] mb-4 bg-neutral-700 text-white focus:outline-none"
               type="text"
               placeholder="Full Name"
+              ref={name}
             />
           )}
           <input
             className="w-full px-5 pt-4 h-[50px] rounded-[4px] mb-4 bg-neutral-700 text-white focus:outline-none"
             type="text"
-            placeholder="Email or phone number"
+            placeholder="Email"
+            ref={email}
           />
           <input
             className="w-full px-5 pt-4 h-[50px] rounded-[4px] mb-4 bg-neutral-700 text-white focus:outline-none"
             type="password"
             placeholder="Password"
+            ref={password}
           />
+          <p className="text-red-500 text-sm">{validationErrorMessage}</p>
           <button
             className="w-full p-4 mt-6 mb-4 text-white bg-red-600 rounded-[4px] font-medium"
             type="submit"
