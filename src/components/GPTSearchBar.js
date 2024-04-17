5d51a4650bcfa88c62c8e8a4b2a7d8922ec31d86
Param Attri <attri.param@gmail.com>
2024-04-17 18:10:28 +0530

    makes tmdb movie api and stores the movie data in redux store

--- src/components/GPTSearchBar.js
+++ src/components/GPTSearchBar.js
@@ -1,8 +1,23 @@
 import { useRef } from "react";
 import openai from "../utils/openai";
+import API_OPTIONS from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addGPTMovieResults } from "../utils/gptSlice";
 
 const GPTSearchBar = () => {
   const searchText = useRef(null);
+  const dispatch = useDispatch();
+
+  const searchMovieTMDB = async (movieName) => {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/search/movie?query=${movieName}&include_adult=false&language=en-US&page=1`,
+      API_OPTIONS
+    );
+
+    const data = await response.json();
+    return data.results;
+  };
+
   const handleGPTSearch = async () => {
     const gptQuery = `Act as a Movie Recommendation system and suggest some movies for the query ${searchText.current.value}. Only give me names of 5 movies, comma separated like the example result given ahead. Example Result: MovieName1, MovieName2, MovieName3, MovieName4, MovieName5`;
 
@@ -12,6 +27,15 @@ const GPTSearchBar = () => {
     });
 
     console.log(gptSearchResults.choices?.[0]?.message?.content);
+    const gptRecommendedMovies =
+      gptSearchResults.choices?.[0]?.message?.content.split(",");
+
+    const moviePromises = gptRecommendedMovies.map((movie) =>
+      searchMovieTMDB(movie)
+    );
+
+    const tmdbMovieResults = await Promise.all(moviePromises);
+    dispatch(addGPTMovieResults({ gptRecommendedMovies, tmdbMovieResults }));
   };
 
   return (
--- src/utils/gptSlice.js
+++ src/utils/gptSlice.js
@@ -4,14 +4,21 @@ const gptSlice = createSlice({
   name: "gpt",
   initialState: {
     showGPTSearch: false,
+    gptRecommendedMovies: null,
+    gptRecommendedMovieResults: null,
   },
   reducers: {
     toggleGPTSearchView: (state) => {
       state.showGPTSearch = !state.showGPTSearch;
     },
+    addGPTMovieResults: (state, action) => {
+      const { gptRecommendedMovies, tmdbMovieResults } = action.payload;
+      state.gptRecommendedMovies = gptRecommendedMovies;
+      state.gptRecommendedMovieResults = tmdbMovieResults;
+    },
   },
 });
 
-export const { toggleGPTSearchView } = gptSlice.actions;
+export const { toggleGPTSearchView, addGPTMovieResults } = gptSlice.actions;
 
 export default gptSlice.reducer;
