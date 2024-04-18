50b0dedd1b1ed98ca37fe260637c8111e5d06398
Param Attri <attri.param@gmail.com>
2024-04-18 18:23:21 +0530

    creates GPT movies suggestion component

--- src/components/GPTMovieSuggestions.js
+++ src/components/GPTMovieSuggestions.js
@@ -1,5 +1,26 @@
+import { useSelector } from "react-redux";
+import MovieList from "./MovieList";
+
 const GPTMovieSuggestions = () => {
-  return <div>GPTMovieSuggestions</div>;
+  const { gptRecommendedMovies, gptRecommendedMovieResults } = useSelector(
+    (store) => store.gpt
+  );
+
+  if (!gptRecommendedMovies) return null;
+
+  return (
+    <div>
+      <div>
+        {gptRecommendedMovies.map((movieName, index) => (
+          <MovieList
+            key={movieName}
+            title={movieName}
+            movies={gptRecommendedMovieResults[index]}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default GPTMovieSuggestions;
